6566cad2dfcb1d9fab46cc8b75b5c7fff570e5b9
SamadRazzak1987 <fa18mcsw0013@maju.edu.pk>
2019-10-26 12:27:30 +0500

    added beauty activity

---
+++ app/src/main/java/com/example/a123/mad_fall19/BeautyScreen.java
@@ -0,0 +1,13 @@
+package com.example.a123.mad_fall19;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class BeautyScreen extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.beauty_screen);
+    }
+}
