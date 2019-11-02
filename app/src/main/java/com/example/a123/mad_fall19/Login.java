64cc89063a37944c30dd660719f97544bb14c05d
SamadRazzak1987 <fa18mcsw0013@maju.edu.pk>
2019-11-02 12:49:33 +0500

    assignment 4 work

---
+++ app/src/main/java/com/example/a123/mad_fall19/Login.java
@@ -0,0 +1,62 @@
+package com.example.a123.mad_fall19;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.content.Intent;
+
+public class Login extends AppCompatActivity {
+
+    EditText email;
+    EditText password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        email = findViewById(R.id.etEmail);
+        password = findViewById(R.id.etPassword);
+    }
+
+    protected void signIn(View v)
+    {
+        String name = email.getText().toString();
+        String pass = password.getText().toString();
+
+        boolean isError= false;
+
+        if((name != null && name.equalsIgnoreCase("")) || !emailValidation(name)){
+            email.setError("Please enter Valid email");
+            isError = true;
+        }
+
+        if(pass != null && pass.equalsIgnoreCase("")){
+            password.setError("Please enter Valid password");
+            isError = true;
+        }
+
+        if(!isError){
+            Intent intent = new Intent(this, beauty.class);
+            intent.putExtra("NAME", name);
+            startActivity(intent);
+        }
+    }
+
+    protected  void signUp(View v)
+    {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    private boolean emailValidation(String str){
+        boolean isValid = false;
+
+        if(str.contains("@"))
+            isValid = true;
+        if(str.length() < 5)
+            isValid = false;
+        return isValid;
+    }
+}
--- app/src/main/java/com/example/a123/mad_fall19/MainActivity.java
+++ app/src/main/java/com/example/a123/mad_fall19/MainActivity.java
@@ -1,13 +1,84 @@
 package com.example.a123.mad_fall19;
 
+<<<<<<< Updated upstream
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+=======
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+>>>>>>> Stashed changes
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText firstname;
+    EditText lastname;
+    EditText gender;
+    EditText country;
+    CheckBox cb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        firstname = findViewById(R.id.etFirstName);
+        lastname = findViewById(R.id.etLastName);
+        gender = findViewById(R.id.etGender);
+        country = findViewById(R.id.etCountry);
+        cb = findViewById(R.id.checkBox);
     }
+<<<<<<< Updated upstream
+=======
+
+    public void sendMessage(View view)
+    {
+        String fname = firstname.getText().toString();
+        String lname = lastname.getText().toString();
+        String gen = gender.getText().toString();
+        String cntry = country.getText().toString();
+        boolean isError= false;
+
+        if(fname != null && fname.equalsIgnoreCase("")){
+            firstname.setError("Please enter Valid first name");
+            isError = true;
+        }
+        if(lname != null && lname.equalsIgnoreCase("")){
+            lastname.setError("Please enter Valid last name");
+            isError = true;
+        }
+        if(gen != null && gen.equalsIgnoreCase("")){
+            gender.setError("Please enter Valid gender");
+            isError = true;
+        }
+        if(cntry != null && cntry.equalsIgnoreCase("")){
+            country.setError("Please enter Valid country");
+            isError = true;
+        }
+        if(!cb.isChecked()){
+            cb.setTextColor(Color.RED);
+            isError = true;
+        }
+
+        if(!isError){
+            Intent intent = new Intent(this, beauty.class);
+            startActivity(intent);
+        }
+    }
+
+    private boolean userNameValidation(String str){
+        boolean isValid = false;
+
+        if(str.contains("@"))
+            isValid = true;
+        if(str.length() < 5)
+            isValid = false;
+        return isValid;
+    }
+>>>>>>> Stashed changes
 }
